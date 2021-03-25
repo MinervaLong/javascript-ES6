c734949ab98b1951b90c997e73431207b5a51ecb
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-03-25 11:10:10 +0100

    Exercise 1 function multiply

--- ecma6.js
+++ ecma6.js
@@ -1,1 +1,11 @@
 // ----------------------------LEVEL 1 | From ES5 to ES6--------------------------------
+
+// EXERCISE 1 | Convert to Arrow functions
+
+/* function multiply(num1, num2) {
+   return num1 * num2;
+}*/
+
+const multiply = (num1, num2) => num1 * num2;
+
+console.log(multiply(2,3));
