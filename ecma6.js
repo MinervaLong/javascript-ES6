5b43c019e34fca81ce804a91858b43677a44f7a4
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-03-26 14:10:50 +0100

    Exercise 8

--- ecma6.js
+++ ecma6.js
@@ -174,7 +174,7 @@ console.log(forEachArray);
 
 // ----------------------------LEVEL 2---------------------------------
 
-// Exercise 7
+// EXERCISE 7 
 
 const multTable = () => {
    let number1;
@@ -197,4 +197,136 @@ const multTable = () => {
 console.log(multTable());
 
 
+// EXERCISE 8
+
+
+// Store elements in variables
+
+const form = document.getElementById('form');
+
+let firstNumber = document.getElementById('firstNumber');
+let secondNumber = document.getElementById('secondNumber');
+
+let num1Error = document.getElementById("num1Error");
+let num2Error = document.getElementById("num2Error");
+
+
+// Functions
+
+const multTable2 = () => {
+   let number1 = parseInt(document.getElementById('firstNumber').value);
+   let number2 = parseInt(document.getElementById('secondNumber').value);
+   let table = document.getElementById('printTable');
+   let i;
+   let j = 0;
+
+   while (table.hasChildNodes()) {
+      table.removeChild(table.firstChild);
+   }
+   
+   if (validate()) {
+      for(i = 1; i <= number2; i++){
+         let tr = document.createElement('tr');
+         tr.className = 'tableRows';
+         let td1 = document.createElement('td');
+         td1.innerHTML = number1;
+         let td2 = document.createElement('td');
+         td2.innerHTML = 'x';
+         let td3 = document.createElement('td');
+         td3.innerHTML = i;
+         let td4 = document.createElement('td');
+         td4.innerHTML = '=';
+         let td5 = document.createElement('td');
+         td5.innerHTML = number1 * i;
+         tr.append(td1,td2,td3,td4,td5);
+         table.append(tr);
+      }
+   }  
+
+}
+
+
+// Next function is a validation once we click in submit button
+const validate = () => {
+   let validation = true; // At first we assume that the data is valid
+
+   // Validate the fields are not empty and are between 1-10
+   if (firstNumber.value == ""){
+      firstNumber.style.border = "3px solid red";
+      num1Error.textContent = "This field cannot be empty";
+      firstNumber.focus();
+      validation = false;
+   }else if (firstNumber.value < 1 || firstNumber.value > 10) {
+      firstNumber.style.border = "3px solid red";
+      //num1Error.textContent = "Numbers between 1 and 10";
+      firstNumber.focus();
+      validation = false;
+   } else {
+      firstNumber.style.border = "1px solid green";
+      num1Error.textContent = "Looks good!";
+   }
+   
+   // The same with secondNumber
+   if (secondNumber.value == ""){
+      secondNumber.style.border = "3px solid red";
+      num2Error.textContent = "This field cannot be empty";
+      secondNumber.focus();
+      validation = false;
+   } else if (secondNumber.value < 1 || secondNumber.value > 10) {
+      secondNumber.style.border = "3px solid red";
+      //num2Error.textContent = "Numbers between 1 and 10";
+      secondNumber.focus();
+      validation = false;
+   } else {
+      secondNumber.style.border = "1px solid green";
+      num2Error.textContent = "Looks good!";
+   }   
+
+   return validation;
+}
+
+// Next functions are validation before we click submit button
+const number1Verify = () => {
+   if (firstNumber.value == ""){
+      firstNumber.style.border = "3px solid red";
+      num1Error.textContent = "This field cannot be empty";
+      firstNumber.focus();
+   }else if (firstNumber.value < 1 || firstNumber.value > 10) {
+      firstNumber.style.border = "3px solid red";
+      num1Error.textContent = "Numbers between 1 and 10";
+      firstNumber.focus();
+   } else {
+      firstNumber.style.border = "1px solid green";
+      num1Error.textContent = "Looks good!";
+   }
+}
+
+const number2Verify = () => {
+   if (secondNumber.value == ""){
+      secondNumber.style.border = "3px solid red";
+      num2Error.textContent = "This field cannot be empty";
+      secondNumber.focus();
+   } else if (secondNumber.value < 1 || secondNumber.value > 10) {
+      secondNumber.style.border = "3px solid red";
+      num2Error.textContent = "Numbers between 1 and 10";
+      secondNumber.focus();
+   } else {
+      secondNumber.style.border = "1px solid green";
+      num2Error.textContent = "Looks good!";
+   }   
+
+}
+
+// Event Listeners
+
+form.addEventListener('submit', multTable2);
+
+// To verify the data before clicking submit
+firstNumber.addEventListener('change', number1Verify);
+secondNumber.addEventListener('change', number2Verify);
+
+// To prevent the form to refresh if it works correctly
+document.getElementById('form').addEventListener('submit', function(e) {
+   e.preventDefault();
+});
  
